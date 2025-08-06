4d7cf7da469893f99699ed8c0467ebd7e18292de
unknown <haskeebullah265@gmail.com>
2025-08-06 07:23:20 +0500

    Added fonts and structured global styles

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -7,18 +7,19 @@ export default function Home() {
       <header>
 
 
-        <div className="navbar container">
-
+        <div className="navbar container flex items-center justify-between">
           
           <div className="logo">
-
-            <Image src= "/img/logo(transparent).png" alt="AghazHub Logo" width={178} height={44} />
-
+            <a href="#"><Image src= "/img/logo.png" alt="AghazHub Logo" width={178} height={0} /></a>
           </div>
 
           
           <nav> {/*These are navlinks */}
-            <ul>
+            <ul className="flex gap-11 sp-bold">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Explore</a></li>
+              <li><a href="#">Contact Us</a></li>
             </ul>
           </nav>
 
