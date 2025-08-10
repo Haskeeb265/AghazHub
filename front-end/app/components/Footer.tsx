7aa85203efa8df25a6209422a4a3a4a293910bd3
Haseebullah <haskeebullah265@gmail.com>
2025-08-10 23:15:02 +0500

    footer changes

--- front-end/app/components/Footer.tsx
+++ front-end/app/components/Footer.tsx
@@ -8,11 +8,12 @@ export default function Footer() {
         <div className="bg-[#F2E2B1] h-200">
             <div className="container mx-auto flex items-center justify-between">
                 <div className="mt-20">
-                <div className="col-1-div">
+                <div className="col-1-div ">
                     <div className="col-1">
-                        <div>
-                            <p className=" text-6xl text-[#fff] sugarpeachy  mb-5">You don't need to scroll anywhere else. <Image className="inline align-middle" src="/img/brown-heart.svg" alt="Brown Heart" width={70} height={70} /> </p> 
+                        <div className="flex items-center">
+                            <p className=" text-5xl text-[#fff] sp-regular neg-5 ">You don't need to scroll anywhere else.</p>  <Image className="pb-2" src="/img/brown-heart.svg" alt="Brown Heart" width={50} height={50} /> 
                         </div>
+                        <p className="sp-regular neg-5 text-[#F88356] mb-5 text-center md:text-start">Everything you need is right here</p>
                     <Link href="#explore" className="text-md sp-bold neg-5 text-[#fff]  bg-[#EF3B51]  rounded-full px-2 py-1 hover:text-[#000] hover:bg-[#F88356] transition-colors duration-300 ease-in-out">Explore Now</Link>
                     </div>
                 </div>
