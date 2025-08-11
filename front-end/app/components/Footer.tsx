edf5cac1f1186146177fcbff0274b494a3c714e2
Haseebullah <haskeebullah265@gmail.com>
2025-08-11 13:16:25 +0500

    Revamped footer

--- front-end/app/components/Footer.tsx
+++ front-end/app/components/Footer.tsx
@@ -1,24 +1,36 @@
 import Image from "next/image"
 import Link from "next/link"
-import { useState } from "react"
 
 export default function Footer() {
     
     return (
-        <div className="bg-[#F2E2B1] h-200">
-            <div className="container mx-auto flex items-center justify-between">
-                <div className="mt-20">
-                <div className="col-1-div ">
-                    <div className="col-1">
-                        <div className="flex items-center">
-                            <p className=" text-5xl text-[#fff] sp-regular neg-5 ">You don't need to scroll anywhere else.</p>  <Image className="pb-2" src="/img/brown-heart.svg" alt="Brown Heart" width={50} height={50} /> 
+    
+        <footer className="bg-[url('/img/footer-img.png')] bg-center bg-cover w-full h-100 py-16">
+            <div className="grid grid-col-2 bg-[#F2E2B1]/50 max-w-[994px] rounded-[150px] mx-auto">
+                
+                <div className="footer-container">
+                    
+                    <div className="col-1 grid grid-row-4">
+                        <div className="row-1">
+                            <Image src="/img/footer-logo.svg" alt="Footer Logo" width={191} height={191} />
                         </div>
-                        <p className="sp-regular neg-5 text-[#F88356] mb-5 text-center md:text-start">Everything you need is right here</p>
-                    <Link href="#explore" className="text-md sp-bold neg-5 text-[#fff]  bg-[#EF3B51]  rounded-full px-2 py-1 hover:text-[#000] hover:bg-[#F88356] transition-colors duration-300 ease-in-out">Explore Now</Link>
+                        
+                        <div className="row-2 flex gap-1">
+                            <p className="sp-regular neg-5 text-[#fff] text-4xl">You don't need to scroll anywhere else</p>
+                            <Image src="/img/brown-heart.svg" alt="brown heart" width={30} height={30} />
+                        </div>
+
+                        <div className="row-3 ">
+                            <Link className="text-base  text-[#fff] px-3 py-2 rounded-full sp-regular neg-5 bg-[#EF3B51]"  href="/explore">Explore <Image className="inline mx-1" src="/img/explore-btn-arrow.svg" alt="213" width={20} height={20}></Image></Link>
+                        </div>
+                    </div>
+
+                    <div className="col-2">
                     </div>
                 </div>
-                </div>
             </div>
-        </div>
+            
+        </footer>
+
     )
 }
